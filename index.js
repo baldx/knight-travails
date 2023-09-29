6534db8e90192938431e3d40bb576e5ed34a5e00
baldx <mr.ahmad.m2006@gmail.com>
2023-09-29 19:44:50 +0200

    create game board

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let gameBoard = [];
-
-class cell {
+class position {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
+let boardSize = 8;
+let gameBoard = [];
 
-function chessBoard () {
-    let isVisited = true;
-
-    
+for (let i = 0; i <= boardSize; i++) {
+    for (let j = 0; j <= i; j++) {
+        gameBoard.push(new position(i, j))
+    }
 }
+console.log(gameBoard);
