9ccede67b1ff9222665027cbdde280636e0638ec
baldx <mr.ahmad.m2006@gmail.com>
2023-10-02 19:10:41 +0200

    add basic first part DFS knight travail

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ for (let i = 0; i <= boardSize; i++) {
 
 function setVisited (position) {
     if (position && typeof position === "object" && "visited" in position) position.visited = true;
-    else return "Invalid position object";
+    else return "Invalid position object" + position;
 
     return position;
 }
@@ -26,8 +26,53 @@ function setVisited (position) {
 function checkValidity (x, y) {
     if (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
         const cell = gameBoard[x * boardSize + y];
-        
+
         if (cell.visited) return null;
         else return cell;
     } else return null;
 }
+
+function findMoves (x, y, boardSize) {
+
+    let validMoves = [];
+
+    const moves = [
+        {dx: 2, dy: 1},
+        {dx: 2, dy: -1},
+        {dx: -2, dy: 1},
+        {dx: -2, dy: -1},
+        {dx: 1, dy: 2},
+        {dx: 1, dy: -2},
+        {dx: -1, dy: 2},
+        {dx: -1, dy: -2}
+    ];
+
+    for (const move of moves) {
+        const newX = x + move.dx;
+        const newY = y + move.dy;
+
+        if (newX >= 0 && newX < boardSize && newY >= 0 && newY < boardSize) {
+            validMoves.push({x: newX, y: newY});
+        }
+    }
+}
+
+function knightTravail (startX, startY, targetX, targetY) {
+
+    if (startX === targetX && startY === targetY) return "Found path";
+
+    let cellVisited = [];
+    cellVisited.push(new Position(startX, startY))
+    cellVisited[0].visited = true;
+
+    while (cellVisited.length !== 0) {
+        let nextCell = cellVisited.pop();
+
+        if (nextCell.x === targetX && nextCell.y === targetY) return "Found path"
+        else nextCell.parent = nextCell;
+
+        knightTravail(nextCell.x, nextCell.y, targetX, targetY);
+    }
+}
+
+console.log(knightTravail(0, 1));
